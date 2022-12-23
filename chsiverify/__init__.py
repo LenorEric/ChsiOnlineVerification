3a5f496b52d36fa40b8a86466639c67df3f29bd5
LenorEric <40317566+LenorEric@users.noreply.github.com>
2022-12-23 12:02:40 +0800

    check verify code

--- chsiverify/__init__.py
+++ chsiverify/__init__.py
@@ -1,3 +1,15 @@
 from .parser import *
 
-__all__ = ["verify_chsi"]
+__all__ = ["verify_chsi", "pre_check_verify_code"]
+
+
+def pre_check_verify_code(verify_code: str) -> bool:
+    if len(verify_code) != 16:
+        return False
+    if verify_code[0] != 'A':
+        return False
+    if not verify_code.isalnum():
+        return False
+    if not verify_code.isupper():
+        return False
+    return True
