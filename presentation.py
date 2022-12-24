7b0ebc7f370a3f4e0926192865b174a70398af74
LenorEric <40317566+LenorEric@users.noreply.github.com>
2022-12-24 23:35:14 +0800

    change presentation

--- presentation.py
+++ presentation.py
@@ -3,4 +3,7 @@ from chsiverify import *
 verify_code = ""  # Enter your verify code here
 
 if __name__ == '__main__':
-    print(verify_chsi(verify_code))
+    result = verify_chsi(verify_code)
+    print(result)
+    print(len(result))
+
